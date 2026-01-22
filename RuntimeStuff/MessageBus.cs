1a36f7909a361b53c1b9360423505d55a4d5fffc
Rudnev Sergey <s.rudnev@miuz.ru>
2026-01-22 13:25:43 +0300

    2026.01.22.1

--- RuntimeStuff/MessageBus.cs
+++ RuntimeStuff/MessageBus.cs
@@ -7,35 +7,148 @@ namespace RuntimeStuff
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Threading;
 
     /// <summary>
-    /// Простейшая реализация шины сообщений (Message Bus)
-    /// для обмена сообщениями внутри процесса.
+    /// Асинхронная потокобезопасная шина сообщений (Message Bus)
+    /// с очередью и пулом рабочих потоков.
     /// </summary>
     /// <remarks>
-    /// Класс позволяет:
+    /// Класс предназначен для обмена сообщениями внутри процесса
+    /// с асинхронной доставкой обработчикам.
+    ///
+    /// Архитектура:
     /// <list type="bullet">
-    /// <item><description>подписываться на сообщения определённого типа;</description></item>
-    /// <item><description>публиковать сообщения и уведомлять всех подписчиков;</description></item>
-    /// <item><description>использовать единый глобальный экземпляр через <see cref="Default"/>.</description></item>
+    /// <item><description>сообщения помещаются в неблокирующую очередь;</description></item>
+    /// <item><description>один или несколько фоновых потоков извлекают сообщения;</description></item>
+    /// <item><description>обработчики вызываются асинхронно относительно издателя.</description></item>
     /// </list>
     ///
-    /// Реализация потокобезопасна на уровне регистрации и доставки сообщений,
-    /// однако обработчики выполняются синхронно в потоке вызова <see cref="Publish{T}"/>.
+    /// Порядок обработки сообщений:
+    /// <list type="bullet">
+    /// <item><description>FIFO на уровне очереди;</description></item>
+    /// <item><description>порядок вызова обработчиков одного сообщения сохраняется;</description></item>
+    /// <item><description>между разными сообщениями возможен параллелизм.</description></item>
+    /// </list>
+    ///
+    /// Исключения, возникающие в обработчиках,
+    /// перехватываются и передаются в <see cref="OnHandlerException"/>,
+    /// не влияя на работу шины.
     /// </remarks>
-    public sealed class MessageBus
+    public sealed class MessageBus : IDisposable
     {
         private readonly ConcurrentDictionary<Type, List<Delegate>> handlers =
             new ConcurrentDictionary<Type, List<Delegate>>();
+
+        private readonly ConcurrentQueue<object> queue = new ConcurrentQueue<object>();
+        private readonly AutoResetEvent signal = new AutoResetEvent(false);
+        private readonly Thread[] workers;
+        private volatile bool running = true;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageBus"/> class.
+        /// Инициализирует новый экземпляр <see cref="MessageBus"/>
+        /// с указанным количеством рабочих потоков.
+        /// </summary>
+        /// <param name="workerCount">
+        /// Количество фоновых потоков,
+        /// обрабатывающих сообщения.
+        /// </param>
+        public MessageBus(int workerCount)
+            : this("MessageBusWorker", workerCount)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageBus"/> class.
+        /// Инициализирует новый экземпляр <see cref="MessageBus"/>
+        /// с настраиваемым именем потоков и количеством воркеров.
+        /// </summary>
+        /// <param name="threadName">
+        /// Базовое имя для рабочих потоков.
+        /// </param>
+        /// <param name="workerCount">
+        /// Количество рабочих потоков.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Генерируется, если <paramref name="workerCount"/> меньше либо равен нулю.
+        /// </exception>
+        public MessageBus(string threadName = "MessageBusWorker", int workerCount = 1)
+        {
+            if (workerCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(workerCount));
+
+            workers = new Thread[workerCount];
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                workers[i] = new Thread(WorkerLoop)
+                {
+                    IsBackground = true,
+                    Name = $"{threadName}-{i}",
+                };
+                workers[i].Start();
+            }
+        }
 
         /// <summary>
         /// Глобальный экземпляр шины сообщений.
         /// </summary>
         /// <remarks>
-        /// Может использоваться как простой singleton
-        /// для обмена сообщениями между компонентами приложения.
+        /// По умолчанию количество рабочих потоков
+        /// равно <see cref="Environment.ProcessorCount"/>.
         /// </remarks>
-        public static MessageBus Default { get; } = new MessageBus();
+        public static MessageBus Default { get; } =
+            new MessageBus(workerCount: Environment.ProcessorCount);
+
+        /// <summary>
+        /// Освобождает ресурсы и корректно завершает работу шины сообщений.
+        /// </summary>
+        /// <remarks>
+        /// Метод:
+        /// <list type="bullet">
+        /// <item><description>останавливает приём новых сообщений;</description></item>
+        /// <item><description>будит все рабочие потоки;</description></item>
+        /// <item><description>дожидается завершения обработки очереди;</description></item>
+        /// <item><description>освобождает системные ресурсы.</description></item>
+        /// </list>
+        ///
+        /// После вызова <see cref="Dispose"/> публикация сообщений невозможна.
+        /// </remarks>
+        public void Dispose()
+        {
+            running = false;
+
+            for (int i = 0; i < workers.Length; i++)
+                signal.Set();
+
+            foreach (var t in workers)
+                t.Join();
+
+            signal.Dispose();
+        }
+
+        /// <summary>
+        /// Публикует сообщение в очередь на асинхронную обработку.
+        /// </summary>
+        /// <typeparam name="T">
+        /// Тип сообщения.
+        /// </typeparam>
+        /// <param name="message">
+        /// Экземпляр сообщения.
+        /// </param>
+        /// <exception cref="ObjectDisposedException">
+        /// Генерируется, если шина сообщений уже остановлена.
+        /// </exception>
+        public void Publish<T>(T message)
+        {
+            if (!running)
+                throw new ObjectDisposedException(nameof(MessageBus));
+
+            queue.Enqueue(message);
+            signal.Set();
+        }
 
         /// <summary>
         /// Подписывает обработчик на сообщения указанного типа.
@@ -44,53 +157,121 @@ namespace RuntimeStuff
         /// Тип сообщения.
         /// </typeparam>
         /// <param name="handler">
-        /// Делегат, который будет вызван при публикации сообщения.
+        /// Делегат-обработчик сообщения.
         /// </param>
-        /// <remarks>
-        /// Обработчики одного типа сообщений вызываются
-        /// в порядке их регистрации.
-        ///
-        /// Метод потокобезопасен.
-        /// </remarks>
         /// <exception cref="ArgumentNullException">
-        /// Может быть сгенерировано при передаче <c>null</c> в качестве <paramref name="handler"/>
-        /// (проверка не выполняется явно).
+        /// Генерируется, если <paramref name="handler"/> равен <c>null</c>.
         /// </exception>
         public void Subscribe<T>(Action<T> handler)
         {
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
             var list = handlers.GetOrAdd(typeof(T), _ => new List<Delegate>());
-            lock (list) list.Add(handler);
+            lock (list)
+            {
+                list.Add(handler);
+            }
         }
 
         /// <summary>
-        /// Публикует сообщение указанного типа и уведомляет всех подписчиков.
+        /// Отписывает обработчик от сообщений указанного типа.
         /// </summary>
         /// <typeparam name="T">
-        /// Тип публикуемого сообщения.
+        /// Тип сообщения.
         /// </typeparam>
-        /// <param name="message">
-        /// Экземпляр сообщения.
+        /// <param name="handler">
+        /// Ранее зарегистрированный обработчик.
         /// </param>
-        /// <remarks>
-        /// Если для данного типа сообщений отсутствуют подписчики,
-        /// метод завершается без каких-либо действий.
-        ///
-        /// Вызов обработчиков выполняется синхронно
-        /// в потоке, из которого был вызван метод.
-        ///
-        /// Исключения, выброшенные обработчиками,
-        /// не перехватываются и прерывают дальнейшую публикацию.
-        /// </remarks>
-        public void Publish<T>(T message)
+        public void Unsubscribe<T>(Action<T> handler)
         {
             if (!handlers.TryGetValue(typeof(T), out var list))
                 return;
 
-            Delegate[] snapshot;
-            lock (list) snapshot = list.ToArray();
+            lock (list)
+            {
+                list.Remove(handler);
+            }
+        }
 
-            for (int i = 0; i < snapshot.Length; i++)
-                ((Action<T>)snapshot[i])(message);
+        /// <summary>
+        /// Обрабатывает исключения, возникшие в обработчиках сообщений.
+        /// </summary>
+        /// <param name="ex">
+        /// Исключение, выброшенное обработчиком.
+        /// </param>
+        /// <param name="message">
+        /// Сообщение, при обработке которого возникло исключение.
+        /// </param>
+        /// <remarks>
+        /// По умолчанию исключения выводятся в <see cref="Debug"/>.
+        /// Метод может быть расширен для логирования или мониторинга.
+        /// </remarks>
+        private static void OnHandlerException(Exception ex, object message)
+        {
+            Debug.WriteLine($"{message}: {ex}");
+        }
+
+        /// <summary>
+        /// Выполняет доставку сообщения всем подписчикам.
+        /// </summary>
+        /// <param name="message">
+        /// Обрабатываемое сообщение.
+        /// </param>
+        private void Dispatch(object message)
+        {
+            var type = message.GetType();
+
+            if (!handlers.TryGetValue(type, out var list))
+                return;
+
+            Delegate[] snapshot;
+            lock (list)
+            {
+                snapshot = list.ToArray();
+            }
+
+            foreach (var handler in snapshot)
+            {
+                try
+                {
+                    handler.DynamicInvoke(message);
+                }
+                catch (Exception ex)
+                {
+                    OnHandlerException(ex, message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Основной цикл рабочего потока.
+        /// </summary>
+        /// <remarks>
+        /// Поток ожидает появления сообщений в очереди,
+        /// извлекает их и передаёт на обработку.
+        ///
+        /// После остановки шины поток корректно
+        /// обрабатывает оставшиеся сообщения в очереди.
+        /// </remarks>
+        private void WorkerLoop()
+        {
+            while (running)
+            {
+                if (!queue.TryDequeue(out var message))
+                {
+                    signal.WaitOne();
+                    continue;
+                }
+
+                Dispatch(message);
+            }
+
+            // обработка оставшихся сообщений
+            while (queue.TryDequeue(out var msg))
+            {
+                Dispatch(msg);
+            }
         }
     }
 }
---
+++ Tests/RuntimeStuff.Tests/MessageBusTests.cs
@@ -0,0 +1,308 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace RuntimeStuff.MSTests
+{
+    [TestClass]
+    public class MessageBusIntegrationTests
+    {
+        [TestMethod]
+        public void StressTest_HighConcurrency_NoDataLoss()
+        {
+            // Arrange
+            using var bus = new MessageBus("StressTest");
+            const int publisherCount = 10;
+            const int messagesPerPublisher = 1000;
+            const int totalMessages = publisherCount * messagesPerPublisher;
+
+            var receivedMessages = new ConcurrentBag<int>();
+            var exceptions = new ConcurrentBag<Exception>();
+            var startSignal = new ManualResetEventSlim(false);
+
+            // Подписываемся
+            bus.Subscribe<int>(msg => receivedMessages.Add(msg));
+
+            // Act - Запускаем несколько издателей
+            var publisherTasks = new Task[publisherCount];
+            for (var i = 0; i < publisherCount; i++)
+            {
+                var publisherId = i;
+                publisherTasks[i] = Task.Run(() =>
+                {
+                    startSignal.Wait();
+
+                    for (var j = 0; j < messagesPerPublisher; j++)
+                    {
+                        try
+                        {
+                            bus.Publish(publisherId * 1000 + j);
+                        }
+                        catch (Exception ex)
+                        {
+                            exceptions.Add(ex);
+                        }
+                    }
+                });
+            }
+
+            // Запускаем всех одновременно
+            startSignal.Set();
+            Task.WaitAll(publisherTasks);
+
+            // Ждем обработки всех сообщений
+            var stopwatch = Stopwatch.StartNew();
+            while (receivedMessages.Count < totalMessages && stopwatch.ElapsedMilliseconds < 10000)
+            {
+                Thread.Sleep(100);
+            }
+
+            // Assert
+            Assert.AreEqual(0, exceptions.Count, $"Exceptions: {exceptions.FirstOrDefault()}");
+            Assert.AreEqual(totalMessages, receivedMessages.Count,
+                $"Expected {totalMessages} messages, received {receivedMessages.Count}");
+        }
+
+        [TestMethod]
+        public void LongRunningHandler_DoesNotBlockOtherMessages()
+        {
+            // Arrange
+            using var bus = new MessageBus("", 2);
+            var fastHandlerCalls = new ConcurrentBag<int>();
+            var slowHandlerStarted = false;
+            var slowHandlerFinished = false;
+
+            // Act
+            // Медленный обработчик
+            bus.Subscribe<int>(msg =>
+            {
+                if (msg == 1)
+                {
+                    slowHandlerStarted = true;
+                    Thread.Sleep(500); // Долгая обработка
+                    slowHandlerFinished = true;
+                }
+            });
+
+            // Быстрый обработчик
+            bus.Subscribe<int>(msg =>
+            {
+                if (msg != 1)
+                {
+                    fastHandlerCalls.Add(msg);
+                }
+            });
+
+            // Отправляем медленное сообщение
+            bus.Publish(1);
+
+            // Ждем начала обработки медленного сообщения
+            Thread.Sleep(100);
+            Assert.IsTrue(slowHandlerStarted);
+
+            // Отправляем быстрые сообщения во время обработки медленного
+            for (var i = 2; i < 10; i++)
+            {
+                bus.Publish(i);
+            }
+
+            // Ждем немного для обработки быстрых сообщений
+            Thread.Sleep(100);
+
+            // Assert - Быстрые сообщения должны быть обработаны, даже пока медленный еще работает
+            Assert.IsFalse(slowHandlerFinished); // Медленный еще не закончил
+            Assert.IsTrue(fastHandlerCalls.Count > 0); // Быстрые уже начали обрабатываться
+        }
+
+        [TestMethod]
+        public void MessageOrdering_PreservedWithinSinglePublisher()
+        {
+            // Arrange
+            using var bus = new MessageBus();
+            var receivedOrder = new ConcurrentQueue<string>();
+            var expectedOrder = new[]
+            {
+                "message1", "message2", "message3", "message4", "message5"
+            };
+
+            // Act
+            bus.Subscribe<string>(msg => receivedOrder.Enqueue(msg));
+
+            foreach (var message in expectedOrder)
+            {
+                bus.Publish(message);
+            }
+
+            // Ждем обработки
+            Thread.Sleep(200);
+
+            // Assert
+            Assert.AreEqual(expectedOrder.Length, receivedOrder.Count);
+            var actualOrder = receivedOrder.ToArray();
+            CollectionAssert.AreEqual(expectedOrder, actualOrder);
+        }
+
+        [TestMethod]
+        public void Memory_Usage_StableUnderLoad()
+        {
+            // Arrange
+            using var bus = new MessageBus();
+            const int iterations = 100000;
+            var largeObject = new byte[1024]; // 1KB
+            var receivedCount = 0;
+
+            bus.Subscribe<byte[]>(msg =>
+            {
+                Interlocked.Increment(ref receivedCount);
+            });
+
+            // Измеряем память до теста
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            var initialMemory = GC.GetTotalMemory(true);
+
+            // Act
+            for (var i = 0; i < iterations; i++)
+            {
+                bus.Publish(largeObject);
+            }
+
+            // Ждем обработки всех сообщений
+            var stopwatch = Stopwatch.StartNew();
+            while (Volatile.Read(ref receivedCount) < iterations && stopwatch.ElapsedMilliseconds < 10000)
+            {
+                Thread.Sleep(10);
+            }
+
+            // Измеряем память после теста
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            var finalMemory = GC.GetTotalMemory(true);
+
+            // Assert
+            var memoryIncrease = finalMemory - initialMemory;
+            Debug.WriteLine($"Memory increase: {memoryIncrease / 1024} KB for {iterations} messages");
+
+            // Память не должна расти линейно с количеством сообщений
+            Assert.IsTrue(memoryIncrease < 50 * 1024 * 1024, // 50MB максимум
+                $"Memory increase too large: {memoryIncrease / 1024 / 1024} MB");
+        }
+
+        [TestMethod]
+        public void ExceptionInHandler_DoesNotCrashBus()
+        {
+            // Arrange
+            using var bus = new MessageBus();
+            var handlerCalls = 0;
+            var exceptionCount = 0;
+
+            // Перехватываем вывод Debug для проверки
+            var originalListeners = Debug.Listeners;
+            var testListener = new TestTraceListener();
+            Debug.Listeners.Add(testListener);
+
+            try
+            {
+                // Act
+                bus.Subscribe<string>(msg =>
+                {
+                    handlerCalls++;
+                    if (handlerCalls == 1)
+                    {
+                        throw new InvalidOperationException("Test exception");
+                    }
+                });
+
+                bus.Subscribe<string>(msg =>
+                {
+                    // Второй обработчик
+                });
+
+                // Отправляем два сообщения
+                bus.Publish("first");
+                bus.Publish("second");
+
+                // Ждем обработки
+                Thread.Sleep(200);
+
+                // Assert - Второе сообщение должно быть обработано, несмотря на исключение при первом
+                Assert.IsTrue(handlerCalls >= 2, $"Handler called {handlerCalls} times, expected at least 2");
+
+                // Проверяем, что исключение было залогировано
+                Assert.IsTrue(testListener.Messages.Any(m => m.Contains("Test exception")));
+            }
+            finally
+            {
+                Debug.Listeners.Remove(testListener);
+            }
+        }
+
+        [TestMethod]
+        public void UnsubscribeAllHandlers_TypeRemovedFromDictionary()
+        {
+            // Arrange
+            using var bus = new MessageBus();
+            Action<string> handler1 = msg => { };
+            Action<string> handler2 = msg => { };
+
+            // Act
+            bus.Subscribe(handler1);
+            bus.Subscribe(handler2);
+
+            // Проверяем, что тип добавлен
+            var handlersField = bus.GetType().GetField("handlers",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var handlers = handlersField?.GetValue(bus) as ConcurrentDictionary<Type, System.Collections.Generic.List<Delegate>>;
+
+            Assert.IsTrue(handlers?.ContainsKey(typeof(string)));
+
+            bus.Unsubscribe(handler1);
+            bus.Unsubscribe(handler2);
+
+            // Assert - После удаления всех обработчиков тип должен быть удален из словаря
+            // Но в текущей реализации список остается пустым, тип не удаляется
+            // Это ожидаемое поведение для текущей реализации
+            Assert.IsTrue(handlers?.ContainsKey(typeof(string)));
+            Assert.AreEqual(0, handlers?[typeof(string)].Count);
+        }
+
+        [TestMethod]
+        public async Task AsyncPattern_Integration()
+        {
+            // Arrange
+            using var bus = new MessageBus();
+            var tcs = new TaskCompletionSource<string>();
+            string receivedMessage = null;
+
+            // Act
+            bus.Subscribe<string>(msg =>
+            {
+                receivedMessage = msg;
+                tcs.SetResult(msg);
+            });
+
+            bus.Publish("async test");
+
+            // Assert
+            var result = await tcs.Task;
+            Assert.AreEqual("async test", result);
+            Assert.AreEqual("async test", receivedMessage);
+        }
+
+        private class TestTraceListener : TraceListener
+        {
+            public ConcurrentBag<string> Messages { get; } = new ConcurrentBag<string>();
+
+            public override void Write(string message)
+            {
+                Messages.Add(message);
+            }
+
+            public override void WriteLine(string message)
+            {
+                Messages.Add(message);
+            }
+        }
+    }
+}
