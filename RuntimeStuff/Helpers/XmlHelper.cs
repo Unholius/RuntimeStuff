02e032648cbc97c43f66784c5a077ab5039c2a4b
Rudnev Sergey <unholius@gmail.com>
2026-02-02 07:34:43 +0300

    dev

---
+++ RuntimeStuff/Helpers/XmlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RuntimeStuff.Helpers
+{
+    public static class XmlHelper
+    {
+        public static string[] GetValues(string xml, string nodeName)
+        {
+            if (string.IsNullOrWhiteSpace(xml) || string.IsNullOrWhiteSpace(nodeName))
+            {
+                return new string[0];
+            }
+
+            try
+            {
+                XDocument doc = XDocument.Parse(xml);
+                return doc.Descendants()
+                          .Where(x => x.Name.LocalName == nodeName)
+                          .Select(x => x.Value)
+                          .ToArray();
+            }
+            catch (Exception)
+            {
+                return new string[0];
+            }
+        }
+    }
+}
---
+++ Tests/RuntimeStuff.Tests/XmlHelperTests.cs
@@ -0,0 +1,28 @@
+﻿using RuntimeStuff.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RuntimeStuff.MSTests
+{
+    [TestClass]
+    public class XmlHelperTests
+    {
+        [TestMethod]
+        public void Test_01()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><multistatus xmlns=\"DAV:\"><response><href>/webdav/s_user2%40deepmail.tokyo/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response><response><href>/webdav/s_user2%40deepmail.tokyo/a5d80190-9e02-4369-8f6e-41923da50c0e/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response><response><href>/webdav/s_user2%40deepmail.tokyo/141dde91-b60a-4f7c-a7c5-e2d84463e92f/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response><response><href>/webdav/s_user2%40deepmail.tokyo/c77a55b9-bd75-4706-acc7-a23c2733e3d9/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response></multistatus>";
+            var values = XmlHelper.GetValues(xml, "response");
+        }
+
+        [TestMethod]
+        public void Test_02()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><multistatus xmlns=\"DAV:\"><response><href>/webdav/s_user2%40deepmail.tokyo/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response><response><href>/webdav/s_user2%40deepmail.tokyo/a5d80190-9e02-4369-8f6e-41923da50c0e/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response><response><href>/webdav/s_user2%40deepmail.tokyo/141dde91-b60a-4f7c-a7c5-e2d84463e92f/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response><response><href>/webdav/s_user2%40deepmail.tokyo/c77a55b9-bd75-4706-acc7-a23c2733e3d9/</href><propstat><prop><max-file-size-bytes>30971000</max-file-size-bytes></prop><status>HTTP/1.1 200 OK</status></propstat></response></multistatus>";
+            var values = XmlHelper.GetValues(xml, "max-file-size-bytes");
+        }
+    }
+}
